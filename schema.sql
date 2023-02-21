d09030ed00c94d32a8b78dfd7c5c85aab94f8be8
harshitajain06 <harshitaforever06@gmail.com>
2023-02-21 17:21:33 +0530

    add data, queries, schema

--- schema.sql
+++ schema.sql
@@ -1,5 +1,6 @@
-/* Database schema to keep the structure of entire database. */
-
-CREATE TABLE animals (
-    name varchar(100)
-);
+CREATE TABLE animals(id serial primary key,
+ name varchar(100), 
+ date_of_birth date, 
+ escape_attempts int, 
+ neutered boolean, 
+ weight_kg decimal(20));
