536a3fcaf331e73e10b6b5ffd680a4fbcf6a1728
harshitajain06 <harshitaforever06@gmail.com>
2023-02-25 16:06:26 +0530

    implemented changes

--- schema.sql
+++ schema.sql
@@ -15,5 +15,5 @@ ALTER TABLE animals ADD COLUMN owner_id INTEGER, ADD CONSTRAINT fk_owner FOREIGN
 
 
 CREATE TABLE vets (id SERIAL PRIMARY KEY, name TEXT, age INTEGER, date_of_graduation DATE);
-CREATE TABLE specializations (vet_id INTEGER, species_id INTEGER, PRIMARY KEY (vet_id, species_id), FOREIGN KEY (vet_id) REFERENCES vets(id), FOREIGN KEY (species_id) REFERENCES species(id));
+CREATE TABLE specializations (vet_id INTEGER, species_id INTEGER, FOREIGN KEY (vet_id) REFERENCES vets(id), FOREIGN KEY (species_id) REFERENCES species(id));
 CREATE TABLE visits (vet_id INTEGER, animal_id INTEGER, visit_date DATE, FOREIGN KEY (vet_id) REFERENCES vets(id), FOREIGN KEY (animal_id) REFERENCES animals(id));
